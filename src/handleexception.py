83025e91782bbc0f24114fd4b9557dbf676f147c
He Zhou <hezho@amazon.com>
2020-03-23 15:00:57 -0500

    modify handle exception 1) Why is the change being made? ---------------------------------
    2) What has changed?
    ---------------------------------
    
    3) Other concerns and/or dependent changes?
    ---------------------------------
    
    4) Related sprint issue, TT, and/or MCM (if exist)
    ---------------------------------
    
    5) Related design/durability review/ORR document link (if applicable)
    ---------------------------------
    
    6) Testing
    ---------------------------------

--- src/handleexception.py
+++ src/handleexception.py
@@ -1,16 +1,21 @@
 import re
 
-def handle(test_list):
+def handle(var):
     alarm = None
     try:
-        for element in test_list:
-            if element == 1:
-                alarm = test_list.index(element)
-        print(alarm)
+        return int(var)
     except ValueError:
         print("metric error")
 
+def throw_exception(var):
+    try:
+        if var == 0:
+            raise ValueError
+    except ValueError:
+        print("f")
+
 if __name__ == '__main__':
-    test_list = [0,2,3,4,5]
-    handle(test_list)
+    # test_list = [0,2,3,4,5]
+    throw_exception(0)
+
 
